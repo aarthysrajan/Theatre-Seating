5ea4ccef9f92809fd06c4ca3dc5a2bbbb332dec5
aarthysrajan <35109602+aarthysrajan@users.noreply.github.com>
2018-05-01 09:24:25 -0400

    Updated comments

--- src/main/java/com/barclays/theatre/seating/model/ReservationState.java
+++ src/main/java/com/barclays/theatre/seating/model/ReservationState.java
@@ -4,6 +4,8 @@
 package com.barclays.theatre.seating.model;
 
 /**
+ * Enumeration of the state of a reservation. While an order is being processed, 
+ * the default state will be REQUESTED.
  * @author Aarthy Rajan
  *
  */
